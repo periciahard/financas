4318aaee4e2e36185e8014e739df14f5b705c10b
periciahard <fcm.fisica@gmail.com>
2026-07-17 19:23:14 -0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-// Preencha apenas se quiser usar sincronização em nuvem.
-// Com url e anonKey vazios, o app funciona normalmente em modo local/offline.
-window.SUPABASE_CONFIG = {
-  url: '',
-  anonKey: ''
-};
